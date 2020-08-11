5ed2be9fe95e3ae5d43b99b52b7f8df985376281
dimple sharma <dimplessharma@hotmail.com>
2020-08-11 10:23:36 -0600

    first commit

---
+++ helloworld.php
@@ -0,0 +1,18 @@
+hello , world ! <? //comments ?>
+more text 
+<?php echo 'This is my standard hello world test using  "echo"';
+// variables start with $ they can not start with number value they are case sensitive.
+$mystring ='hello this is my string !!';
+echo( $mystring );
+
+// re assigning is as simple as assigning . 
+$mystring = 'my string is now changed \n';
+echo( $mystring );
+$mysingleqoatestring = 'hello there with single quotes ' ;
+$mydoubleqoatestring = "hello there with single quotes " ;
+echo( $mysingleqoatestring );
+echo( $mydoubleqoatestring);
+
+
+
+
